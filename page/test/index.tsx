6924f62a6e46482bdebe2cc65dfef21caf362b20
lq513 <1345016958@qq.com>
2024-11-07 16:07:45 +0800

    MOD

--- page/test/index.tsx
+++ page/test/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from 'react';
-import { Button } from 'antd-mobile';
+import { Button, Collapse } from 'antd-mobile';
+import axios from 'axios';
 import { FormattedMessage } from 'react-intl';
 
 // 测试css是否被tree shaking
@@ -16,8 +17,22 @@ import text from 'assets/1.txt';
 
 const Test = () => {
   const [time, setTime] = useState(new Date().toLocaleString());
+  const [ip, setIP] = useState('loading...');
+  const [timeZone, setTimeZone] = useState('loading...');
   const timer = useRef<number>();
   useEffect(() => {
+    try {
+      setTimeZone(Intl.DateTimeFormat().resolvedOptions().timeZone);
+    } catch (error) {
+      console.warn('The browser does not support timezone detection. Using default timezone.');
+    }
+    
+    axios('https://api.ipify.org')
+      // .then(response => response.text())
+      .then(res => {
+        setIP(res.data);
+        console.log('Public IP Address:', res.data);
+      }).catch(e => console.log(e));
     // 测试cros跨域
     // fetch('http://localhost:3002/getJsDoc').then(res => {
     //   console.log(res);
@@ -36,9 +51,19 @@ const Test = () => {
       <h2 className="title">
         <FormattedMessage id="title" />
       </h2>
-      <p className="paragraph">{text.toString()}</p>
-      <div id="ignore" style={{ padding: '10px', lineHeight: '1.1', color: 'var(--grey-9-rgb)' }}>测试css</div>
-      <Button color="warning" className="btn">test antd</Button>
+      <p className="paragraph">
+        {text.toString()}
+        <br />
+        时区：{timeZone}
+        <br />
+        ip：{ip}
+      </p>
+      <Collapse>
+          <Collapse.Panel key='1' title='test'>
+            <div id="ignore" style={{ padding: '10px', lineHeight: '1.1', color: 'var(--grey-9-rgb)' }}>测试css</div>
+            <Button color="warning" className="btn">test antd</Button>
+          </Collapse.Panel>
+      </Collapse>
       <div className="footer">
         <div>{time}</div>
         {/* <a id="ignore1">webpack | </a>
